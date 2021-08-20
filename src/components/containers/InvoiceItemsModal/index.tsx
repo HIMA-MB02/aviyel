9189e4e7ca0d148dc906c6f6e0733143dc6f6266
HIMA-MB02 <himanshu.ganpa@gmail.com>
2021-08-20 12:33:11 +0530

    search bar text and modal width

--- src/components/containers/InvoiceItemsModal/index.tsx
+++ src/components/containers/InvoiceItemsModal/index.tsx
@@ -5,11 +5,14 @@ import Modal from '../../atoms/Modal';
 import { IInvoiceItemsModal } from './types';
 import InvoiceItemsModalTable from '../InvoiceItemsModalTable';
 import InvoiceTotalsModal from '../InvoiceTotalsModal';
+import { useSelector } from 'react-redux';
+import { ReduxState } from '../../../redux';
 
 const InvoiceItemsModal: React.FunctionComponent<IInvoiceItemsModal> = ({
     id,
     title
 }) => {
+    const [name, email] = useSelector((state: ReduxState) => [state.invoiceReducer.formData.name, state.invoiceReducer.formData.email]);
     return (
         <Modal id={id} title={title}>
             <div>
@@ -18,9 +21,9 @@ const InvoiceItemsModal: React.FunctionComponent<IInvoiceItemsModal> = ({
                     <div className='customer'>
                         <h6 className='text-muted'>CUSTOMER DETAILS</h6>
                         <h6 className='text-black customer-name-text'>
-                            Himanhshu Ganapavarpu
+                            {name}
                         </h6>
-                        <h6 className='text-muted'>himanshu.ganpa@gmail.com</h6>
+                        <h6 className='text-muted'>{email}</h6>
                     </div>
                 </div>
                 <hr />
